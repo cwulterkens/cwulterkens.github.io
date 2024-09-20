5e145ae20d95547b66dd66e7354fa6833a15da36
500137660 <500137660@fvtc.edu>
2024-09-20 09:45:13 -0500

    localStorage usage

--- js/site.js
+++ js/site.js
@@ -1,1 +1,5 @@
+// Your existing code
 console.log("Hello World");
+
+// Adding the secret message to localStorage
+localStorage.setItem("It's a secret to everybody.", "It's dangerous to go alone! Take this.");
